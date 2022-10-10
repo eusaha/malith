1c83b43aa4f16b3ee434e13893b1671595f330d8
eusaha <114933587+eusaha@users.noreply.github.com>
2022-10-10 16:02:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+<h1 align="center">🍭 𝗭𝗲𝗿𝗼 𝗧𝘄𝗼 𝗠𝗗 🍭<br></h1>
+
+<p align="center">
+
+  <img src="https://user-images.githubusercontent.com/104565822/177012232-97866d97-0798-4ca2-9e56-5d0a2413642b.png" alt="21-akeno-wallpaper-Images-of-Akeno-Himejima-Wallpaper-industrious-info" />
+
+</p>
+
+
+<p align="center"> 
+
+  A Moduler WhatsApp Bot designed for both PM and Groups - To take your boring WhatsApp usage into a whole different level.
+
+   
+###  “This Userbot is no longer supported, please consider using [Secktor-MD](https://github.com/CitelVoid/Secktor-Md) instead.”
+
+
+
+### How to enable Zero-Two On your number
+-   First Open `Github Link Of Zero Two`
+-   Click on Fork Button upwards right corner
+-  Tap Deploy Image down blew and Fill config vars
+-   Tap on Deploy button and wait 1 min
+-   Tap manage and go on `Resources` and turn on web dynos,tap on open app and Scan Qr code from WhatsApp.
+-   After Scanning you can use [Kaffeine](https://kaffeine.herokuapp.com/) or just switch to worker from web.
+-   Bot is Working Now,Enjoy ♥️
+
+- `Thank me later.`
+# Main Features of Bot
+- PM Permit (Upcoming)
+- First Whatsapp Bot with `Auto Nsfw detection with percentage`
+- First whatsapp bot with `NLP AI`
+- text to Sticker(like Quotely Telegram Bot)
+- Scribd link unlocker
+- Torrent Search
+- Translator for any language
+- Textmaker
+- Nsfw
+- Group Features
+- Chat Features
+- Switches for Turning NSFW on and off
+- Random Anime
+- Anime, Character Search
+- Reaction Commands like Poke,punch,bonk
+- Mongodb Support
+- YouTube,Fb,Twitter,tiktok downloader
+- iplookup,imdb,sticker,emojimix, ebinary,dbinary
+- And list goes on + adding commands daily + we add requested features too
+- Check usage of Commands by prefix+command help eg- -scribd help
+
+  
+[![](https://raw.githubusercontent.com/ZeroTwoInc/Media/main/logo/UPPER.png)](https://ZeroTwoMd.tech/#qrcode)
+  
+[![Deploy](https://raw.githubusercontent.com/ZeroTwoInc/Media/main/logo/MIDDLE.png)](https://heroku.com/deploy?template=https://github.com/Sampandey001/Zero-Two-Md)
+
+[![](https://raw.githubusercontent.com/ZeroTwoInc/Media/main/logo/LOWER.png)](https://youtu.be/rqbeusycfHU)
+
+## Whatsapp Group
+
+<a href="https://chat.whatsapp.com/KK6AVKEwPVJ0aXoWo2cK2g"><img src="https://img.shields.io/badge/Join Group-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+<a href="https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbCl"><img src="https://img.shields.io/badge/Join Group-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+## This bot is mainly created on prefix - if you change this buttons will not work
+
+
+### The Hard Method (Not Recommend)
+```js
+apt update
+apt upgrade
+pkg update && pkg upgrade
+pkg install bash
+pkg install libwebp
+pkg install git -y
+pkg install nodejs -y 
+pkg install ffmpeg -y 
+pkg install wget
+pkg install imagemagick -y
+git clone https://github.com/Sampandey001/Zero-Two-Md
+cd Zero-Two-Md
+npm start
+```
+
+### ⚠️ Warning! 
+```
+ This bot is not a official bot by Whatsapp INC So if you want to use it then feel free to do so.
+ Your WhatsApp Number can be banned by Whatsapp as it is not official
+ Zero-Two-MD bot is still in the development stage, so there can be few bugs.Feel free to report that to us.
+```
+
+## Thanks for Everything 
+- [XEON](https://github.com/dgxeon)
+
+
+
+
+## Developers
+
+[![XEON](https://github.com/dgxeon.png?size=80)](https://github.com/dgxeon) | [![Fantox](https://github.com/fantox001.png?size=100)](https://github.com/fantox001) | [![Sam Pandey](https://github.com/sampandey001.png?size=109)](https://github.com/sampandey001) | [![ISSA](https://github.com/issa2001.png?size=80)](https://github.com/issa2001) | [![Prince-Mendiratta](https://github.com/Prince-Mendiratta.png?size=80)](https://github.com/Prince-Mendiratta)
+----|----|----|----|----
+[XEON](https://wa.me/916909137213) | [Fantox](https://wa.me/918101187835) | [Sam Pandey](https://wa.me/919628516236) | [Issa](https://wa.me/254115175696) | [Prince-Mendiratta](https://wa.me/917838204238)
+Base Bot  | Developer, Bug Fixes, Modules |Developer,Bug Fixes,Base, Modules, | Modules, Idea | Modules
+
+
+
+## License
+This project is protected by `GNU General Public Licence v3.0` license.
+
+
+
+
+## Contributing
+Feel free to contribute.
+
+### Disclaimer
+This is an open source WhatsApp bot based on Multi Device created by [`@SamPandey001`](https://github.com/SamPandey001) & [`@fantox`](https://github.com/FantoX001) working with [`@adiwajshing/baileys`](https://github.com/adiwajshing/baileys). Do not recode it without notifying the owners otherwise you will be petitioned for fraud & copyright infringement!
+
+## Help
+Give a ⭐ to this repo if it helped you.
---
+++ config.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const chalk = require('chalk')
+//git test by Sam    huh
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+global.thum = fs.readFileSync("./ZeroTwoMedia/image3.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./ZeroTwoMedia/image3.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./ZeroTwoMedia/image3.jpg") //ur error pic
+global.thumb = fs.readFileSync("./ZeroTwoMedia/image3.jpg")
+
+global.ytname = "YT: Zero Two Multi Device"
+global.location = "Sultanpur, Uttar Pradesh, India"
+global.botscript = "https://github.com/SamPandey001/Zero-Two-Md"
+global.antitags = true
+global.rkyt = []
+global.banUser = []
+global.banchat = []
+global.ntilink = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.wlcm = []
+global.gcrevoke = []
+global.apikey = process.env.HEROKU_API_KEY_NEW || 'NO-API'
+global.appname = process.env.HEROKUAPP_NAME || 'NO-Name'
+var _0x1a32e8=_0x41cc;function _0x41cc(_0x40a8d6,_0x3701e2){var _0x2635b7=_0x2635();return _0x41cc=function(_0x41cc3a,_0x231162){_0x41cc3a=_0x41cc3a-0x1ee;var _0x1a7446=_0x2635b7[_0x41cc3a];return _0x1a7446;},_0x41cc(_0x40a8d6,_0x3701e2);}(function(_0x1ecbf2,_0x7bdf04){var _0x5aa09a=_0x41cc,_0x3df619=_0x1ecbf2();while(!![]){try{var _0x5cc6d3=-parseInt(_0x5aa09a(0x1f7))/0x1*(-parseInt(_0x5aa09a(0x208))/0x2)+-parseInt(_0x5aa09a(0x20a))/0x3+parseInt(_0x5aa09a(0x1fe))/0x4+-parseInt(_0x5aa09a(0x213))/0x5*(parseInt(_0x5aa09a(0x214))/0x6)+-parseInt(_0x5aa09a(0x1ef))/0x7+parseInt(_0x5aa09a(0x1f4))/0x8*(-parseInt(_0x5aa09a(0x1f1))/0x9)+-parseInt(_0x5aa09a(0x207))/0xa*(-parseInt(_0x5aa09a(0x211))/0xb);if(_0x5cc6d3===_0x7bdf04)break;else _0x3df619['push'](_0x3df619['shift']());}catch(_0x2d5d82){_0x3df619['push'](_0x3df619['shift']());}}}(_0x2635,0xb11de),global[_0x1a32e8(0x203)]={'zenz':_0x1a32e8(0x1f3),'amel':_0x1a32e8(0x1fa),'bx':_0x1a32e8(0x201),'hardianto':_0x1a32e8(0x20e),'jonaz':_0x1a32e8(0x1fc),'neoxr':_0x1a32e8(0x212),'xteam':_0x1a32e8(0x1ee),'nzcha':_0x1a32e8(0x206),'bg':_0x1a32e8(0x204),'fdci':_0x1a32e8(0x1f6),'rey':'https://server-api-rey.herokuapp.com','dzx':_0x1a32e8(0x1f9),'bsbt':_0x1a32e8(0x1fb),'zahir':_0x1a32e8(0x1f5),'zeks':_0x1a32e8(0x210),'zekais':_0x1a32e8(0x1f2),'hardianto':_0x1a32e8(0x202),'pencarikode':_0x1a32e8(0x20f),'erdwepe':_0x1a32e8(0x209),'lolhuman':'https://api.lolhuman.xyz','LeysCoder':_0x1a32e8(0x200)},global[_0x1a32e8(0x1ff)]={'https://zenzapis.xyz':_0x1a32e8(0x20d),'https://melcanz.com':_0x1a32e8(0x205),'https://hardianto.xyz':'hardianto','https://api.xteam.xyz':'Dawnfrostkey','https://zahirr-web.herokuapp.com':_0x1a32e8(0x20b),'https://bsbt-api-rest.herokuapp.com':'benniismael','https://server-api-rey.herokuapp.com':_0x1a32e8(0x20c),'https://api.zeks.xyz':'apivinz','https://hardianto-chan.herokuapp.com':'hardianto','https://pencarikode.xyz':_0x1a32e8(0x1f0),'https://leyscoders-api.herokuapp.com':_0x1a32e8(0x1fd),'https://zekais-api.herokuapp.com':_0x1a32e8(0x1f8),'https://api.lolhuman.xyz':'pelitbetsihluwh'});function _0x2635(){var _0x254d9e=['APIKeys','https://leyscoders-api.herokuapp.com','https://bx-hunter.herokuapp.com','https://hardianto-chan.herokuapp.com','APIs','http://bochil.ddns.net','elaina','http://nzcha-apii.herokuapp.com','23240MWEtdw','2mgCnbp','https://erdwpe-api.herokuapp.com','4057560PxhLYH','zahirgans','apirey','4ad2c1556c','https://hardianto.xyz','https://pencarikode.xyz','https://api.zeks.xyz','12881pMnlwi','https://neoxr-api.herokuapp.com','5455EMlIaQ','618qCuUOC','https://api.xteam.xyz','6307182QLPFhR','pais','5333697XNqdoy','http://zekais-api.herokuapp.com','https://zenzapis.xyz','16SRFgTg','https://zahirr-web.herokuapp.com','https://api.fdci.se','544763FimhMg','zekais','https://api.dhamzxploit.my.id','https://melcanz.com','https://bsbt-api-rest.herokuapp.com','https://jonaz-api-v2.herokuapp.com','dappakntlll','4041952CDMDxA'];_0x2635=function(){return _0x254d9e;};return _0x2635();}
+//═══════[modification]════════\\
+global.owner = process.env.owner || ['919628516236','916307151530','918953092716' , '919330953026' , '918101187835'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['919628516236' , '918101187835'] //another owner number
+global.premium = ['919628516236' , '918101187835'] //premium number
+global.pengguna = '𝐙𝐞𝐫𝐨 𝐓𝐰𝐨 𝐌𝐝' //username
+global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+
+global.port = process.env.PORT || 5000
+global.botnma = process.env.botnma || '𝐙𝐞𝐫𝐨 𝐓𝐰𝐨 𝐌𝐝'//bot name
+global.watermark = "ENJOY WITH ZERO TWO"
+global.botname = process.env.botnamw || '𝐙𝐞𝐫𝐨 𝐓𝐰𝐨 𝐌𝐝'
+global.ownernma = 'Ikshwaku Pandey' //owner name
+global.ownername = process.env.ownername || 'Ikshwaku Pandey' //owner name
+global.packname = 'Zero-Two' //sticker package name
+global.author = 'Dont Steal' //sticker author name
+global.sessionNamew = 'session.sam'
+global.sessionName = process.env.SESSION_ID || 'No-Session'
+global.zerotwo = "https://github.com/SamPandey001/Zero-Two-MD" //ur gc link
+global.zerotwo2 = "https://github.com/SamPandey001/Zero-Two-MD" //ur gc link2
+global.websitex = "https://ZeroTwoMd.tech" //ur website
+global.prefa = ['-'] //prefix
+global.sp = '✨' //design
+module.exports = {
+  HEROKU: {
+        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
+        API_KEY: process.env.HEROKU_API_KEY === undefined ? '7328bcf1-4e08-459b-804f-1c35483b9112' : process.env.HEROKU_API_KEY,
+        APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'quiet-harbor-86010' : process.env.HEROKU_APP_NAME
+    },
+  BRANCH: 'franxx',
+  VERSION: process.env.VERSION === undefined ? 'v.1.8' : process.env.VERSION,
+ LANG: process.env.LANGUAGE === undefined ? 'en' : process.env.LANGUAGE.toUpperCase(),
+ SUPPORT: "120363040838753957@g.us",
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
+};
+global.limitawal = {
+    premium: "Infinity", //premium user limit
+    free: 40 //free user limit
+}
+function _0x541c(){var _0x520861=['998048IAMZZZ','./assets/img/zt.jpg','thumb','777904mDNRjN','676040CCHFKt','readFileSync','967962kDYBDh','2833800DPpmXk','1317540Ofobcc','1314yvlPXk','2933gQtPbt'];_0x541c=function(){return _0x520861;};return _0x541c();}function _0x58cd(_0x281a3c,_0x3b6c20){var _0x541cd5=_0x541c();return _0x58cd=function(_0x58cd30,_0x40480a){_0x58cd30=_0x58cd30-0xaf;var _0x2d9baf=_0x541cd5[_0x58cd30];return _0x2d9baf;},_0x58cd(_0x281a3c,_0x3b6c20);}var _0x325c36=_0x58cd;(function(_0x2da43c,_0x5f3f1a){var _0x4e00f1=_0x58cd,_0x4299cb=_0x2da43c();while(!![]){try{var _0x15c880=parseInt(_0x4e00f1(0xb7))/0x1+-parseInt(_0x4e00f1(0xb4))/0x2+-parseInt(_0x4e00f1(0xaf))/0x3+parseInt(_0x4e00f1(0xb0))/0x4+-parseInt(_0x4e00f1(0xb1))/0x5+-parseInt(_0x4e00f1(0xb2))/0x6*(-parseInt(_0x4e00f1(0xb3))/0x7)+-parseInt(_0x4e00f1(0xb8))/0x8;if(_0x15c880===_0x5f3f1a)break;else _0x4299cb['push'](_0x4299cb['shift']());}catch(_0x4ed7c3){_0x4299cb['push'](_0x4299cb['shift']());}}}(_0x541c,0x63b68),global[_0x325c36(0xb6)]=fs[_0x325c36(0xb9)](_0x325c36(0xb5)));
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+	require(file)
+})
